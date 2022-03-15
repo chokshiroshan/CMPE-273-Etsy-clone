0121e962f49be866842f126feba5f36c3d868f3c
Roshan <roshan.chokshi@sjsu.edu>
2022-03-15 08:44:37 -0700

    added backend(sign in) and database.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
+const session = require("express-session");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,23 +11,96 @@ const db = mysql.createConnection({
   database: "lab",
 });
 
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
 db.connect((err) => {
   if (err) {
     throw err;
   }
-  console.log("db connected!");
 });
 
-app.get("/login", (req, res) => {
-  let sql = "SELECT username, password FROM users";
-  let query = db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.json(result[0]);
-  });
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/login", function (request, response) {
+  // Capture the input fields
+  let username = request.body.username;
+  let password = request.body.password;
+  console.log(request.body);
+  // Ensure the input fields exists and are not empty
+  if (username && password) {
+    // Execute SQL query that'll select the account from the database based on the specified username and password
+    db.query(
+      "SELECT * FROM users WHERE username = ? AND password = ?",
+      [username, password],
+      function (error, results, fields) {
+        // If there is an issue with the query, output the error
+        if (error) throw error;
+        // If the account exists
+        if (results.length > 0) {
+          // Authenticate the user
+          response.writeHead(200, {
+            "Content-Type": "text/plain",
+          });
+          // Redirect to home page
+          console.log("Correct");
+          response.end("Correct Username and/or Password!");
+        } else {
+          console.log("Incorrect");
+
+          response.end("UNSUCCESS");
+        }
+        response.end();
+      }
+    );
+  } else {
+    response.send("Please enter Username and Password!");
+    response.end();
+  }
 });
 
-app.listen("3001", () => {
-  console.log("hey");
+app.post("/register", function (request, response) {
+  // Capture the input fields
+  let username = request.body.username;
+  let password = request.body.password;
+  console.log(request.body);
+  // Ensure the input fields exists and are not empty
+  if (username && password) {
+    // Execute SQL query that'll select the account from the database based on the specified username and password
+    db.query(
+      "SELECT * FROM users WHERE username = ? AND password = ?",
+      [username, password],
+      function (error, results, fields) {
+        // If there is an issue with the query, output the error
+        if (error) throw error;
+        // If the account exists
+        if (results.length > 0) {
+          // Authenticate the user
+          response.writeHead(200, {
+            "Content-Type": "text/plain",
+          });
+          // Redirect to home page
+          console.log("Correct");
+          response.end("Correct Username and/or Password!");
+        } else {
+          console.log("Incorrect");
+
+          response.end("UNSUCCESS");
+        }
+        response.end();
+      }
+    );
+  } else {
+    response.send("Please enter Username and Password!");
+    response.end();
+  }
 });
+
+app.listen("3001", () => {});
